9b4cf20a6fda57b56b137ace5ab9809ca0ff9a64
陈智力 <chzhlo@gmail.com>
2019-10-25 11:39:10 +0800

    4：SPL标准库；链式操作；魔术方法

---
+++ Common/Database.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Common;
+
+
+class Database
+{
+    function where($where){
+        return $this;
+    }
+
+    function order($order){
+        return $this;
+    }
+
+    function limit($limit){
+        return $this;
+    }
+}
--- Common/Object.php
+++ Common/Object.php
@@ -1,10 +1,40 @@
 <?php
-
+//魔术方法
 namespace Common;
 class Object
 {
-    static function test()
+    protected $array=array();
+    function __set($name, $value)
     {
+        var_dump(__METHOD__);
+        $this->array[$name]=$value;
+    }
 
+    function __get($name)
+    {
+        var_dump(__METHOD__);
+        return $this->array[$name];
+    }
+
+    // function __call($name, $arguments)
+    // {
+    //     var_dump($name,$arguments);
+    //     return "magic fuction\n";
+    // }
+
+    static function __callStatic($name, $arguments)
+    {
+        var_dump($name,$arguments);
+        return "magic fuction\n";
+    }
+
+    function __toString()
+    {
+        return __CLASS__;
+    }
+
+    function __invoke()
+    {
+        return "invoke";
     }
 }
